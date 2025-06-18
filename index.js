ff250aed6cccf2e590d8c4822deb0652afbf27f0
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-06-18 10:35:01 +0600

    my 2nd commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log(7);
 console.log("islamsoriful.hero");
+console.log('good morning');
+console.log(57);
+
+var BananaPrice = (12);
+console.log(BananaPrice);
+
+var Country = ('Bangladesh,India,China,Pakistan,Japan,Nepal,Bhutan,Nederland');
+console.log(Country);

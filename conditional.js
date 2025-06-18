110f99c63d6796a901c3b59dc944e2cbf095b298
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-06-18 15:29:31 +0600

    my 3rd commit

---
+++ conditional.js
@@ -0,0 +1,21 @@
+var biscuitPrice = 8;
+if(biscuitPrice < 10){
+    console.log("Ami biscuits khabo");
+}
+else{
+    console.log("onno kichu khabo")
+
+}
+
+
+var jopPaiso = true;
+var savingAmount = 5000;
+if(jopPaiso == true && savingAmount > 200000){
+    console.log('biye hoye jabe')
+}
+else if(jopPaiso == true){
+    console.log('biye hobe kina dekche wait koro')
+}
+else{
+    console.log("biye hobe na")
+}
--- index.js
+++ index.js
@@ -8,3 +8,24 @@ console.log(BananaPrice);
 
 var Country = ('Bangladesh,India,China,Pakistan,Japan,Nepal,Bhutan,Nederland');
 console.log(Country);
+
+var isHold = true;
+var isGold = false;
+console.log(isGold);
+
+// String, Number, Boolean
+
+var userDetails = "I am A JarNalis MY JOB IS Teacher";
+console.log(userDetails.toUpperCase());
+
+var naturName = "kalam ali jamal khan";
+console.log(naturName.indexOf('ali'));
+
+var number1 = 25;
+var number2 = '15.5';
+number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+var number = 5.09999;
+var result = Math.random(number);
+console.log(result);
